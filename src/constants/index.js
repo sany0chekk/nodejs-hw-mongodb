6ef1801ef13df7add3ad05aed429396c56f7238f
Oleksandr <olexandr.veseluy@gmail.com>
2024-09-29 20:25:20 +0300

    add resetPass

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,7 @@
+import path from 'node:path';
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -5,3 +9,11 @@ export const SORT_ORDER = {
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+};
